81fee57b2b7dfd137114c9c6f18c5e8d1f28efcc
Mark Moses <mark@panjiva.com>
2016-11-29 14:42:14 -0500

    Add in date display

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -4,8 +4,6 @@ class ResultsController < ApplicationController
   def create
     max_count = 3
     win_count = params[:win_count].to_i
-    puts "*** result #{params[:result][:teams]}"
-    puts "** WINCOUNT #{win_count}"
     lose_count = max_count-win_count
     for i in 0...win_count
       response = ResultService.create(@game, params[:result])
